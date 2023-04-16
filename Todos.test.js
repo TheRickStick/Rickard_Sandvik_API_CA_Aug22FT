9d7ab7ab2b69c7b13a31a3a0c1abb30207eba0cc
TheRickStick <114582372+TheRickStick@users.noreply.github.com>
2023-04-16 22:45:30 +0200

    iii

--- Todos.test.js
+++ Todos.test.js
@@ -5,14 +5,16 @@ require('dotenv').config()
 const bodyParser = require("body-parser");
 
 const authRoutes = require("./routes/auth");
+const todoRoutes = require("./routes/todo");
 const username = process.env.USERNAME;
 const password = process.env.PASSWORD;
 
 app.use(bodyParser.json());
 
 app.use("/", authRoutes);
+app.use("/todo", todoRoutes);
 
-describe("testing-guest-routes", () => {
+describe("testing-todo-routes", () => {
   let token;
 
   test("POST /login - success", async () => {
@@ -27,7 +29,7 @@ describe("testing-guest-routes", () => {
     console.log(token);
   });
 
-  test("GET / - success", async () => {
+  test("GET /todo - success", async () => {
     const body  = await request(app).get("/todo").set("Authorization", `Bearer ${token}`);
     console.log(body);
     expect(body).toHaveProperty("status", "success");
@@ -35,6 +37,47 @@ describe("testing-guest-routes", () => {
     expect(body.data).toBeInstanceOf(Array);
   });
 
+  test("POST /todo - success", async () => {
+    const todo = {
+      title: "Test Todo",
+      description: "Test description",
+      completed: false
+    };
+    const body  = await request(app).post("/todo").set("Authorization", `Bearer ${token}`).send(todo);
+    console.log(body);
+    expect(body).toHaveProperty("status", "success");
+    expect(body).toHaveProperty("data");
+    expect(body.data).toHaveProperty("_id");
+  });
 
+  test("DELETE /todo/:id - success", async () => {
+    const todo = {
+      title: "Test Todo",
+      description: "Test description",
+      completed: false
+    };
+    const { body: { data } } = await request(app).post("/todo").set("Authorization", `Bearer ${token}`).send(todo);
+    const body  = await request(app).delete(`/todo/${data._id}`).set("Authorization", `Bearer ${token}`);
+    console.log(body);
+    expect(body).toHaveProperty("status", "success");
+    expect(body).toHaveProperty("data");
+    expect(body.data).toHaveProperty("_id", data._id);
+  });
+
+  test("GET /todo - error without token", async () => {
+    const body  = await request(app).get("/todo");
+    console.log(body);
+    expect(body).toHaveProperty("status", "error");
+    expect(body).toHaveProperty("message", "Authentication failed");
+  });
+
+  test("GET /todo - error with invalid token", async () => {
+    const body  = await request(app).get("/todo").set("Authorization", `Bearer invalid-token`);
+    console.log(body);
+    expect(body).toHaveProperty("status", "error");
+    expect(body).toHaveProperty("message", "Authentication failed");
+  });
 
 });
+
+
