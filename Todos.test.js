766978db9fc4f804e1e00081751daae5329e740c
TheRickStick <114582372+TheRickStick@users.noreply.github.com>
2023-04-16 11:15:16 +0200

    ppp

--- Todos.test.js
+++ Todos.test.js
@@ -3,78 +3,48 @@ const request = require("supertest");
 const app = express();
 require('dotenv').config()
 const bodyParser = require("body-parser");
+
 const authRoutes = require("./routes/auth");
-const db = require("./models");
-const UserService = require("./services/UserService");
-const userService = new UserService(db);
-const { TodoService } = require("./services/TodoService"); // Import TodoService
-//const todoService = new TodoService(db); // Initialize TodoService
+
 app.use(bodyParser.json());
+
 app.use("/", authRoutes);
 
-describe("testing-auth-routes", () => {
+describe("testing-guest-routes", () => {
   let token;
-
-  beforeAll(async () => {
-    await userService.create("testuser", "test@test.com", "test");
-  });
-
 
   test("POST /signup - success", async () => {
     const credentials = {
-      name: "testuser2",
-      email: "test2@test.com",
+      name: "testuser",
+      email: "test@test.com",
       password: "test"
-    };
+    }
     const { body } = await request(app).post("/signup").send(credentials);
-    expect(body).toHaveProperty("status", "success");
-    expect(body).toHaveProperty("data.result", "You created an account.");
+    expect(body).toHaveProperty("data");
+    expect(body.data).toHaveProperty("result");
+    expect(body.data.result).toBe("You created an account.")
   });
 
   test("POST /login - success", async () => {
     const credentials = {
       email: "test@test.com",
       password: "test"
-    };
+    }
     const { body } = await request(app).post("/login").send(credentials);
-    expect(body).toHaveProperty("status", "success");
-    expect(body).toHaveProperty("data.token");
-    token = body.data.token;
+    expect(body).toHaveProperty("data");
+    expect(body.data).toHaveProperty("token");
+    token = body.data.token
+    console.log(token)
   });
 
-  test("POST /login - invalid credentials", async () => {
+
+  test("DELETE / - success", async () => {
     const credentials = {
       email: "test@test.com",
-      password: "invalid"
-    };
-    const { body } = await request(app).post("/login").send(credentials);
-    expect(body).toHaveProperty("status", "fail");
-    expect(body).toHaveProperty("data.result", "Incorrect email or password");
+    }
+    const { body } = await request(app).delete("/").send(credentials);
+    expect(body).toHaveProperty("data");
+    expect(body.data).toHaveProperty("result");
+    expect(body.data.result).toBe("You deleted an account.")
   });
-
-  test("POST /login - missing credentials", async () => {
-    const credentials = {};
-    const { body } = await request(app).post("/login").send(credentials);
-    expect(body).toHaveProperty("status", "fail");
-    expect(body).toHaveProperty("data.email", "Email and password are required.");
-  });
-
-  test("POST /login - email not found", async () => {
-    const credentials = {
-      email: "notfound@test.com",
-      password: "test"
-    };
-    const { body } = await request(app).post("/login").send(credentials);
-    expect(body).toHaveProperty("status", "fail");
-    expect(body).toHaveProperty("data.result", "Incorrect email or password");
-  });
-
-  test("GET /todos - success", async () => {
-    const { body } = await request(app)
-      .get("/todos")
-      .set("Authorization", `Bearer ${token}`);
-    expect(body).toHaveProperty("status", "success");
-    expect(body).toHaveProperty("data.todos");
-  });
-
 });
